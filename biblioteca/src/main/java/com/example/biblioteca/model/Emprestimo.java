29ff648a61e344ef8bbd90dc1b7505de592226dc
Scythe <scythe.github@gmail.com>
2024-10-10 20:47:01 -0300

    Add files via upload

---
+++ biblioteca/src/main/java/com/example/biblioteca/model/Emprestimo.java
@@ -0,0 +1,70 @@
+package com.example.biblioteca.model;
+
+import com.example.biblioteca.model.Exemplar;
+import com.example.biblioteca.model.Cliente;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "emprestimos")
+public class Emprestimo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exemplar_id", nullable = false)
+    private Exemplar exemplar;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+    @Column(name = "data", nullable = false)
+    private LocalDate data;
+
+    public Emprestimo() {
+    }
+
+    public Emprestimo(Integer id, Exemplar exemplar, Cliente cliente, LocalDate data) {
+        this.id = id;
+        this.exemplar = exemplar;
+        this.cliente = cliente;
+        this.data = data;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Exemplar getExemplar() {
+        return exemplar;
+    }
+
+    public void setExemplar(Exemplar exemplar) {
+        this.exemplar = exemplar;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+}
---
+++ biblioteca/src/main/java/com/example/biblioteca/model/Livro.java
@@ -0,0 +1,91 @@
+package com.example.biblioteca.model;
+
+import com.example.biblioteca.enums.Categoria;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "nome", nullable = false, length = 255)
+    private String nome;
+
+    @Column(name = "autor", nullable = false, length = 255)
+    private String autor;
+
+    @Column(name = "ano_publicacao", nullable = false)
+    private int anoPublicacao;
+
+    // Alteração para String devido ao tamanho do ISBN
+    @Column(name = "isbn", nullable = false, length = 13)
+    private String isbn;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "categoria", nullable = false)
+    private Categoria categoria;
+
+    public Livro() {
+    }
+
+    public Livro(Integer id, String nome, String autor, int anoPublicacao, String isbn, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.isbn = isbn;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
